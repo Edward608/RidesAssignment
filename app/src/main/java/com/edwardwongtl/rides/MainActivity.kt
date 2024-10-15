2fb236d3ba50380a9525e4ec4a3ba83656eee8fe
Edward Wong <edwardwongtl@gmail.com>
2024-10-15 07:46:37 -0600

    Code clean up

--- app/src/main/java/com/edwardwongtl/rides/MainActivity.kt
+++ app/src/main/java/com/edwardwongtl/rides/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.edwardwongtl.rides
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
--- app/src/main/java/com/edwardwongtl/rides/ui/fragment/VehicleDetailFragment.kt
+++ app/src/main/java/com/edwardwongtl/rides/ui/fragment/VehicleDetailFragment.kt
@@ -5,11 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
-import com.edwardwongtl.rides.MainActivity
 import com.edwardwongtl.rides.databinding.FragmentVehicleDetailBinding
 
 class VehicleDetailFragment : Fragment() {
@@ -24,13 +20,6 @@ class VehicleDetailFragment : Fragment() {
         with(binding) {
             lifecycleOwner = this@VehicleDetailFragment
             vehicle = args.vehicle
-//            (requireActivity() as MainActivity).apply {
-//                setSupportActionBar(toolbar)
-//                supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//                val navController = findNavController()
-//                val appConfiguration = AppBarConfiguration(navController.graph)
-//                toolbar.setupWithNavController(navController, appConfiguration)
-//            }
         }
 
         return binding.root
